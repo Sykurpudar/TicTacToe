8d48dd7c47ed9e14fb79a0ecbc6a0a9f43510392
asdiseinars <asdis@eythor.is>
2018-10-22 15:46:19 +0000

    Setting up CircleCI

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 //Copy from lab
-const app = require("./src/api");
+/*const app = require("./src/server/api");
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
  console.log("Server running on port " + PORT);
+});*/
+
+const express = require("express");
+const app = express();
+const api = require("./src/server/api");
+
+// For all queries to localhost:3000/api/...
+// use the API router (see below)
+app.use("/api", api);
+
+// For any other route (URL) just send an error
+app.get("*", (req, res) => {
+  res.send({ error: "No route defined" });
 });
+
+module.exports = app;

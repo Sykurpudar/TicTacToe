c233a5fd7025ec00c26e612da7887702e2ec375a
Þórður Atlason <thorduratl17@ru.is>
2018-10-25 13:42:47 +0000

    HTML and Logic connected through index.js

--- src/logic/game.js
+++ src/logic/game.js
@@ -4,11 +4,16 @@
 const Player = require("./player.js");
 
 class Game {
+
 	constructor(playerX, playerO, currentPlayer) {
 		this.playerX = playerX;
 		this.playerO = playerO;
 		this.currentPlayer = currentPlayer;
-  }
+  	}
+
+  	makeMove(HTMLgrid){
+  		HTMLgrid[4].innerHTML = 'Nice';
+  	}
 }
 
 module.exports = Game;

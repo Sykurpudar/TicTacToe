32f35b33ad7f79ce7dc00d35a24c1916dd0f7086
Ivar Loftsson <ivar15@ru.is>
2018-10-27 16:44:14 +0000

    Added get and inc in scoreboard.js and can now print score

--- src/logic/scoreBoard.js
+++ src/logic/scoreBoard.js
@@ -7,6 +7,22 @@ class ScoreBoard {
 		this.xPoints = 0;
 		this.oPoints = 0;
 	}
+
+	getXPoints() {
+		return this.xPoints;
+	}
+
+	getOPoints() {
+		return this.oPoints;
+	}
+
+	incrementX() {
+		this.xPoints++;
+	}
+
+	incrementO() {
+		this.oPoints++;
+	}
 }
 
 module.exports = ScoreBoard;
--- src/logic/scoreBoard.test.js
+++ src/logic/scoreBoard.test.js
@@ -8,3 +8,25 @@ it('ScoreBoard constructor sets the correct values', () => {
   expect(obj.xPoints).toBe(0);
   expect(obj.oPoints).toBe(0);
 });
+
+test("Check score for X", () => {
+	const testScore = new ScoreBoard();
+	expect(testScore.getXPoints()).toBe(0);
+});
+
+test("Check score for O", () => {
+	const testScore = new ScoreBoard();
+	expect(testScore.getOPoints()).toBe(0);
+});
+
+test("Check increment for X", () => {
+	const testScore = new ScoreBoard();
+	testScore.incrementX();
+	expect(testScore.getXPoints()).toBe(1);
+});
+
+test("Check increment for O", () => {
+	const testScore = new ScoreBoard();
+	testScore.incrementX();
+	expect(testScore.getXPoints()).toBe(1);
+});

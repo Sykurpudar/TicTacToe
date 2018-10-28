cf236fb0a0f1f3e19d971cab4bc12585a24f3c91
Birgitta <birgittab15@ru.is>
2018-10-28 16:03:44 +0000

    getting artifacts to work

--- toPDF.js
+++ toPDF.js
@@ -1,15 +1,15 @@
 var markdownpdf = require("markdown-pdf")
 
  
-markdownpdf().from("./docs/design_report.md").to("./docs/design_report_pdf.pdf", function () {
+markdownpdf().from("./docs/design_report.md").to("./artifacts/design_report_pdf.pdf", function () {
   console.log("Done")
 })
 
-markdownpdf().from("./docs/administration_manual.md").to("./docs/administration_manual_pdf.pdf", function () {
+markdownpdf().from("./docs/administration_manual.md").to("./artifacts/administration_manual_pdf.pdf", function () {
   console.log("Done")
 })
 
-markdownpdf().from("./docs/development_manual.md").to("./docs/development_manual_pdf.pdf", function () {
+markdownpdf().from("./docs/development_manual.md").to("./artifacts/development_manual_pdf.pdf", function () {
   console.log("Done")
 })
 

0e402c33c3d6432da70910ede172235f7764b3e8
Birgitta <birgittab15@ru.is>
2018-10-28 14:59:15 +0000

    adding build script for converting to pdf

--- docs/toPDF.js
+++ docs/toPDF.js
@@ -1,15 +1,15 @@
 var markdownpdf = require("markdown-pdf")
 
  
-markdownpdf().from("./design_report.md").to("./design_report_pdf.pdf", function () {
+markdownpdf().from("./docs/design_report.md").to("./docs/design_report_pdf.pdf", function () {
   console.log("Done")
 })
 
-markdownpdf().from("./administration_manual.md").to("./administration_manual_pdf.pdf", function () {
+markdownpdf().from("./docs/administration_manual.md").to("./docs/administration_manual_pdf.pdf", function () {
   console.log("Done")
 })
 
-markdownpdf().from("./development_manual.md").to("./development_manual_pdf.pdf", function () {
+markdownpdf().from("./docs/development_manual.md").to("./docs/development_manual_pdf.pdf", function () {
   console.log("Done")
 })
 

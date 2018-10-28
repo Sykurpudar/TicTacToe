381d4b72819c9af8215fff0952adf3ee143c9c03
Jóhann Ívar Björnsson <johann.b@agrdynamics.com>
2018-10-28 16:38:39 +0000

    Test that board is cleard upon clicking play again

--- src/server/checkResponse.test.js
+++ src/server/checkResponse.test.js
@@ -6,14 +6,14 @@ describe("TicTacToe", () => {
   let url = "http://sykurflautur.herokuapp.com";
 
   beforeEach(async () => {
-    browser = await puppeteer.launch({ headless: false, slowMo: 0 });
+    browser = await puppeteer.launch({ headless: true, slowMo: 0 });
     page = await browser.newPage();
   });
 
   afterEach(() => {
-    browser.close();
+    //browser.close();
   });
-  /*
+  
 
   test("Server responds with 200 status code", async () => {
     const response = await page.goto(url);
@@ -28,19 +28,47 @@ describe("TicTacToe", () => {
     expect(pageTitle).toBe("Greeting page");
   });
 
-  */
-  
   test("Clicking top left square puts an X in it", async () => {
     await page.goto(url);
     let response = await page.goto(url);
-
     await page.click('.top.right');
-
-    const pageTitle = await page.title();
 
    const inner_html = await ( await ( await page.$( '.top.right' ) ).getProperty( 'innerHTML' ) ).jsonValue();
   
    expect(inner_html).toBe("X");
 
+  }); 
+
+  test("Clicking Play again clears the board", async () => {
+    await page.goto(url);
+    let response = await page.goto(url);
+
+    await page.evaluate(() => {
+      let squares = document.getElementsByClassName('square');
+      for (let s of squares)
+          s.click();
+    });
+
+    await page.click('#play-again');
+
+    var topleft = await ( await ( await page.$( '.top.left' ) ).getProperty( 'innerHTML' ) ).jsonValue();
+    var topmid = await ( await ( await page.$( '.top.midcol' ) ).getProperty( 'innerHTML' ) ).jsonValue();
+    var topright = await ( await ( await page.$( '.top.right' ) ).getProperty( 'innerHTML' ) ).jsonValue();
+
+    var midleft = await ( await ( await page.$( '.middle.left' ) ).getProperty( 'innerHTML' ) ).jsonValue();
+    var midmid = await ( await ( await page.$( '.middle.midcol' ) ).getProperty( 'innerHTML' ) ).jsonValue();
+    var midright = await ( await ( await page.$( '.middle.right' ) ).getProperty( 'innerHTML' ) ).jsonValue();
+
+    var botleft = await ( await ( await page.$( '.bottom.left' ) ).getProperty( 'innerHTML' ) ).jsonValue();
+    var botmid = await ( await ( await page.$( '.bottom.midcol' ) ).getProperty( 'innerHTML' ) ).jsonValue();
+    var botright = await ( await ( await page.$( '.bottom.right' ) ).getProperty( 'innerHTML' ) ).jsonValue();
+
+    inner_html = topleft + topmid + topright + midleft + midmid + midright + botleft + botmid + botright;
+    expect(inner_html).toBe("");
+
   });
+
+
+
+
 });

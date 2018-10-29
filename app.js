f7242b130b9fc7b11b14ec279536d112b82c0e86
Þórður Atlason <thorduratl17@ru.is>
2018-10-29 00:01:02 +0000

    Final finishing up

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use("/api", api);
 app.use(express.static(__dirname +'/public/'));
 
 app.get("/test/", (req, res) => {
-  res.redirect('/testResults/lcov-report/index.html');
+  res.redirect('https://sykur-testresults.herokuapp.com/');
 });
 
 
